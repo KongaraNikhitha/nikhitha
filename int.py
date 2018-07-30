89600ecc7cd2755ac8ca2004c5d778fa8106c7bd
KongaraNikhitha <41769749+KongaraNikhitha@users.noreply.github.com>
2018-07-30 15:28:35 +0530

    Update int.py

--- int.py
+++ int.py
@@ -1,9 +1,9 @@
 n=int(intput())
-if(n<1 or (n%1)!=0)
-print("wrong input")
+if(n<1 or (n%1)!=0):
+  print("wrong input")
 else:
-temp=0
+  temp=0
 while(n!=0):
-temp=temp+1
+  temp=temp+1
 n=n-1
 print(temp)
