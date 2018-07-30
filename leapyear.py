b9db3cd8ef103834feb3519fe8be27a08fe69a43
KongaraNikhitha <41769749+KongaraNikhitha@users.noreply.github.com>
2018-07-30 14:39:44 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,8 +1,9 @@
 a=int(input())
 if(a%4=0):
-print("leapyear")
+  print("leapyear")
 elif(a%400=0):
-print("leapyear")
+  print("leapyear")
 elif(a%100!):
+  print("leapyear")
 else:
-print("not leapyear")
+  print("not leapyear")
