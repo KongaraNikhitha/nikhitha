a047145000005f97c61250acf3d7d8edba0c182e
KongaraNikhitha <41769749+KongaraNikhitha@users.noreply.github.com>
2018-08-08 16:22:40 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,9 +1,9 @@
 =[int(input()
 if(n<=1000):
-for i in range(2,n):
-if(n%i)==0:
-print("yes")
-break
-else:
-print("no")
+      for i in range(2,n):
+      if(n%i)==0:
+      print("yes")
+      break
+ else:
+      print("no")
 
