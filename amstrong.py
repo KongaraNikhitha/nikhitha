08499f4e52fb1747270feecca079cba5ab74d01e
KongaraNikhitha <41769749+KongaraNikhitha@users.noreply.github.com>
2018-08-09 14:34:18 +0530

    Update amstrong.py

--- amstrong.py
+++ amstrong.py
@@ -2,10 +2,10 @@ num=int(input())
 sum=0
 temp=n
 while(temp>0):
-dig=temp%10
-sum+=dig**3
-temp//=10
+  dig=temp%10
+  sum+=dig**3
+  temp//=10
 if num==sum:
-print("yes")
+  print("yes")
 else:
-print("no")
+  print("no")
