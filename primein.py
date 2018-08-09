1360266c5f4308ab360297cf7729b6d8e43e8664
KongaraNikhitha <41769749+KongaraNikhitha@users.noreply.github.com>
2018-08-09 13:15:09 +0530

    Update primein.py

--- primein.py
+++ primein.py
@@ -1,9 +1,10 @@
 m,n=map(int,raw_input().split())
-for num in range(m,n):
-  c=0
-  for i in range(m,n+1):
-    if(num%i==0):
-      if(c==2):
-        print num,
+for i in range(m+1,n):
+	if(i>1):
+		for j in range(2,i):
+			if(i%j==0):
+    				  break
+		else:
+			  print i,
 
 
