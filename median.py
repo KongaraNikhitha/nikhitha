e26dd7118f19e3cbe6526a452b0f4a967762d974
KongaraNikhitha <41769749+KongaraNikhitha@users.noreply.github.com>
2018-08-14 14:45:36 +0530

    Update median.py

--- median.py
+++ median.py
@@ -5,6 +5,6 @@ def median(l,n):
   else:
     return (l[n/2-1]+l[n/2])/2
   n=int(raw_input())
-  l=[int(x) for x in raw_input ().split()]
+  l=[int(p) for p in raw_input ().split()]
   print(median(l,n-1))
 
