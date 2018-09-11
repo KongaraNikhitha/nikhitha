4ca6e946d7006ef8adfccffe6bcbc41243b62695
KongaraNikhitha <41769749+KongaraNikhitha@users.noreply.github.com>
2018-09-11 19:40:39 +0530

    Update 42.py

--- 42.py
+++ 42.py
@@ -2,7 +2,12 @@ s1,s2=map(str,raw_input().split())
 p1=len(s1)
 p2=len(s2)
 if p1==p2:
-  print s1 or s2
+  if s1==s2:
+    print s1
+  elif s1>s2:
+    print s1
+  else:
+    print s2
 elif p1>p2:
   print s1
 else:
