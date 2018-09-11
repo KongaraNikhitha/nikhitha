2f7ec7049115369a68f3e01c384e4f4e4bc60f95
KongaraNikhitha <41769749+KongaraNikhitha@users.noreply.github.com>
2018-09-11 18:43:00 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -3,11 +3,11 @@ n1=1
 n2=1
 count=0
 if nterms==1:
-print(n1)
+  print(n1)
 else:
-whilw count<nterms
-print n1,
-nth=n1+n2
-n1=n2
-n2=nth
-count+=1
+  while count<nterms:
+      print n1,
+      nth=n1+n2
+      n1=n2
+      n2=nth
+      count+=1
